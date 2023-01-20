f1d603c5d3ad63c15996e0144362c4d0b40b91a7
Carlos <kip4akz@gmail.com>
2023-01-20 15:41:11 +0600

    testet new get parameters with specific id

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'devOps' },
+        { id: 4, title: 'QA' }
+    ]
+};
 app.get('/', (req, res) => {
     const a = 4;
     if (a > 5) {
@@ -15,11 +23,19 @@ app.get('/', (req, res) => {
         res.send('Hello World!');
     }
 });
-app.get('/about', (req, res) => {
-    res.send('./pages/about.html');
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses;
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title) > -1);
+    }
+    res.json(foundCourses);
 });
-app.post('/about', (req, res) => {
-    res.send('Wo added some info!!!');
+app.get('/courses/:id', (req, res) => {
+    const foundCours = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCours) {
+        res.sendStatus(404);
+    }
+    res.json(foundCours);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import express, { Request, Response } from 'express'
+
 const app = express()
 const port = 3000
+
+type CourseType = {
+  id: number
+  title: string
+}
+
+type dateBase = {
+  courses: Array<CourseType>
+}
+
+const db: dateBase = {
+  courses: [
+    {id:1, title: 'front-end'},
+    {id:2, title: 'back-end'},
+    {id:3, title: 'devOps'},
+    {id:4, title: 'QA'}
+  ]
+}
 
 app.get('/', (req: Request, res: Response) => {
   const a = 4
@@ -11,13 +30,24 @@ app.get('/', (req: Request, res: Response) => {
   }
 })
 
-app.get('/about', (req: Request, res: Response) => {
-  res.send('./pages/about.html')
+app.get('/courses', (req: Request, res: Response) => {
+  let foundCourses = db.courses
+  if(req.query.title) {
+    foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title as string) > -1)
+  }
+
+  res.json(foundCourses)
 })
 
-app.post('/about', (req: Request, res: Response) => {
-  res.send('Wo added some info!!!')
+app.get('/courses/:id', (req: Request, res: Response) => {
+  const foundCours = db.courses.find(c => c.id === +req.params.id)
+  if(!foundCours) {
+    res.sendStatus(404)
+  }
+  
+  res.json(foundCours)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
